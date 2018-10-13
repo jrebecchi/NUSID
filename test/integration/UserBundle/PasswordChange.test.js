6d12b473e66c63f96f47dbc63a9e547cad6abf99
JohannC <johann.corcuff@gmail.com>
2018-10-13 18:30:15 -0300

    New test cases ffor user password modification integration test

--- test/integration/UserBundle/PasswordChange.test.js
+++ test/integration/UserBundle/PasswordChange.test.js
@@ -45,6 +45,46 @@ describe('Test Modifying password', () => {
         })
         .then((response) => {
             expect(response.statusCode).toBe(200);
+            return request.post('/modify-password').send({old_password: "Wrongpassword!", password: passwordDesired, confirm_password: passwordDesired});
+        })
+        .then((response) => {
+            expect(response.statusCode).toBe(302);
+            expect(response.header.location).toBe("/settings");
+            return request.get('/settings');
+        })
+        .then((response) => {
+            expect(response.statusCode).toBe(200);
+            expect(response.text.includes("You entered a wrong password")).toBeTruthy();
+            return request.get('/logout');
+        })
+        .then((response) => {
+            expect(response.statusCode).toBe(302);
+            expect(response.header.location).toBe('/');
+            return request.post('/login').send({username: testUser.email, password: testUser.password})
+        })
+        .then((response) => {
+            expect(response.statusCode).toBe(302);
+            expect(response.header.location.includes("dashboard")).toBeTruthy();
+            return request.post('/modify-password').send({old_password: testUser.password, password: passwordDesired, confirm_password: passwordDesired+"mistake"});
+        })
+        .then((response) => {
+            expect(response.statusCode).toBe(302);
+            expect(response.header.location).toBe("/settings");
+            return request.get('/settings');
+        })
+        .then((response) => {
+            expect(response.statusCode).toBe(200);
+            expect(response.text.includes("error")).toBeTruthy();
+            return request.get('/logout');
+        })
+        .then((response) => {
+            expect(response.statusCode).toBe(302);
+            expect(response.header.location).toBe('/');
+            return request.post('/login').send({username: testUser.email, password: testUser.password})
+        })
+        .then((response) => {
+            expect(response.statusCode).toBe(302);
+            expect(response.header.location).toBe('/dashboard');
             return request.post('/modify-password').send({old_password: testUser.password, password: passwordDesired, confirm_password: passwordDesired});
         })
         .then((response) => {
